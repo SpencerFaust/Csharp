ca8397e8b72d08641f1796ddaf6dbba32dc75f5c
Spencer Faust <faus0059@umn.edu>
2019-04-07 16:33:03 -0500

    Did a few more instructionals.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace myApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World" + DateTime.Now);
+
+            int a = 9;
+            int b = 9;
+            int c = a + b;
+            Console.WriteLine(c);
         }
     }
 }
